3e57452021b513e4c2d777e897ee8bc8e36fa1a1
MakerJoseph <1joelmaker@gmail.com>
2018-07-24 16:03:47 +0300

    creating the final list of various engineering disciplines

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 $(document).ready(function(){
-  $('button#btnind').click(function(){
-    $('#workspace').hide();
+  $('img#indoor').click(function(){
+    $('.workspace').hide();
     $('#factory-office').show();
-  })
+  });
 
-})
+  $('img#outdoor').click(function(){
+    $('.workspace').hide();
+    $('.fieldwork').show();
+  });
+
+  $('img#field1').click(function(){
+    $('.workspace').hide();
+    $('.fieldwork').hide();
+    $('#ans1').show();
+  });
+
+  $('img#field2').click(function(){
+    $('.workspace').hide();
+    $('.fieldwork').hide();
+    $('#ans2').show();
+  });
+
+  $('img#fact').click(function(){
+    $('.workspace').hide();
+    $('#factory-office').hide();
+    $('#ans3').show();
+  });
+
+  $('img#office').click(function(){
+    $('.workspace').hide();
+    $('#factory-office').hide();
+    $('#ans4').show();
+  });
+
+});
