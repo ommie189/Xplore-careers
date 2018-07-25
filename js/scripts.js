86fcef8bb8f5de14ae78ad5ababdb9e86722ad23
Ommie189 <naomikuhi189@gmail.com>
2018-07-25 10:13:21 +0300

    added logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,134 @@
 $(document).ready(function() {
-  $("#C1").click(function() {
-    var list = "<li>" + "pharmacy" + "</li>"
-    $("#list").append(list);
+  $('a#A1').click(function() {
+    $('Q2').show();
+    $('Q1').hide();
+    var list = '<li>' + 'pharmacy' + '</li>'
+    $('#list').append(list);
   })
 
+  $('a#A2').click(function(){
+      $('#Q2').show();
+      $('#Q1').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#A3').click(function(){
+      $('#Q2').show();
+      $('#Q1').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#A4').click(function(){
+      $('#Q2').show();
+      $('#Q1').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#A5').click(function(){
+      $('#Q2').show();
+      $('#Q1').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#A6').click(function(){
+      $('#Q2').show();
+      $('#Q1').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+
+    $('a#B1').click(function(){
+      $('#Q3').show();
+      $('#Q2').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#B2').click(function(){
+      $('#Q3').show();
+      $('#Q2').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#B3').click(function(){
+      $('#Q3').show();
+      $('#Q2').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#B4').click(function(){
+      $('#Q3').show();
+      $('#Q2').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#C1').click(function(){
+      $('#Q4').show();
+      $('#Q3').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#C2').click(function(){
+      $('#Q4').show();
+      $('#Q3').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#C3').click(function(){
+      $('#Q4').show();
+      $('#Q3').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#C4').click(function(){
+      $('#Q4').show();
+      $('#Q3').hide();
+      var list='<li>'+'Commerce'+'</li>'
+      $('#list').append(list);
+    })
+    $('a#D1').click(function(){
+      $('#Q5').show();
+      $('#Q4').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+
+    $('a#D2').click(function(){
+      $('#Q5').show();
+      $('#Q4').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+
+    $('a#D3').click(function(){
+      $('#Q5').show();
+      $('#Q4').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#D4').click(function(){
+      $('#Q5').show();
+      $('#Q4').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#D5').click(function(){
+      $('#Q5').show();
+      $('#Q4').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#E1').click(function(){
+      $('#Q6').show();
+      $('#Q5').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append(list);
+    })
+    $('a#E2').click(function(){
+      $('#Q6').show();
+      $('#Q5').hide();
+      var list='<li>'+''+'</li>'
+      $('#list').append1`;
+    })
 
 
 })
